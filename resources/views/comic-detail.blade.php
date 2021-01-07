5caa6569cf9ec5c0d55fc31efd1141d85f1f5e90
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-01-07 17:36:00 +0100

    comic main content layout

--- resources/views/comic-detail.blade.php
+++ resources/views/comic-detail.blade.php
@@ -3,17 +3,22 @@
 @section('content')
 <main class="comic-main">
   {{-- Hero --}}
-  <div class="hero" style="background-image: url({{ $comic['image-hero'] }});">
+  <div class="hero mb-3" style="background-image: url({{ $comic['image-hero'] }});">
     <div class="container-small">
       <img src="{{ $comic['image-cover'] }}" alt="Cover home">
     </div>
   </div>
-  <div class="container-small">
-    {{-- Comic detail section --}}
+
+  {{-- Comic detail section --}}
+  <div class="container-small flex jc-between">
     <section class="comic-detail">
-      <h2>{{ $comic['title'] }}</h2>
-      <div class="price">
-        {{ $comic['price'] }}
+      <h2 class="mb-2">{{ $comic['title'] }}</h2>
+      <div class="price flex mb-2">
+        <div class="flex jc-between">
+          <span>U.S. Price: ${{ $comic['price'] }}</span>
+          <span>AVAILABLE ON 11/10</span>
+        </div>
+        <div>Check Availability</div>
       </div>
       <div class="description">
         {!! $comic['body'] !!}
@@ -21,8 +26,43 @@
     </section>
     {{-- Advertisement --}}
     <div class="adv">
+      <h4 class="text-right mb-1">ADVERTISEMENT</h4>
       <img src="{{ asset('images/adv.png') }}" alt="adv">
     </div>
   </div>
+
+  {{-- Comic info section --}}
+  <section class="comic-info">
+    <div class="container-small flex jc-between">
+      {{-- Talent info --}}
+      <div class="info">
+        <h3 class="mb-2">Talent</h3>
+        <div class="info-box flex highlight">
+          <h5>Art by:</h5>
+          <span>Sandy Jarrell, Agnes Garbowska</span>
+        </div>
+        <div class="info-box flex highlight">
+          <h5>Written by:</h5>
+          <span>P.C. Morrisey, Heather Nuhfer</span>
+        </div>
+      </div>
+      {{-- Specs info --}}
+      <div class="info">
+        <h3 class="mb-2">Specs</h3>
+        <div class="info-box flex highlight">
+          <h5>Series:</h5>
+          <span>{{ $comic['title'] }}</span>
+        </div>
+        <div class="info-box flex">
+          <h5>U.S. Price:</h5>
+          <span>{{ $comic['price'] }}</span>
+        </div>
+        <div class="info-box flex">
+          <h5>On Sale Date:</h5>
+          <span>Nov 10 2020</span>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
 @endsection
