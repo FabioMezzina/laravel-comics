aab084d2c8ade66b117b9de393bdbb9ca134765f
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-01-07 15:43:48 +0100

    home controller + show comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,18 @@
 @extends('layouts.main')
+
+@section('content')
+<main class="home-main">
+  <div class="container">
+    <ul class="comic-list flex f-wrap">
+      @foreach($comics as $comic)
+      <li class="comic-item">
+        <a href="#">
+            <img src="{{ $comic['image'] }}" alt="{{ $comic['title'] }}">
+          <h3>{{ $comic['title'] }}</h3>
+        </a>
+      </li>
+      @endforeach
+    </ul>
+  </div>
+</main>
+@endsection
