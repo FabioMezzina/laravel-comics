b0aaa9858fbe4ae5d5c3059bc7fe2ccf8a613994
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-01-07 15:14:09 +0100

    repo init + site header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,87 @@
+<header class="dc-header">
+  {{-- Top header --}}
+  <section class="top-header">
+    <div class="container">
+      <ul class="flex jc-end">
+        <li>
+          <a href="#">DC POWER VISA</a>
+        </li>
+        <li>
+          <a href="#">ADDITIONAL DC SITES</a>
+        </li>
+      </ul>
+    </div>
+  </section>
+  {{-- Bottom header --}}
+  <nav class="bottom-header">
+    <div class="container flex jc-between ai-center">
+      {{-- DC Logo --}}
+      <div class="wrapper-logo">
+        <a href="/">
+          <img src="{{ asset('images/logo.png') }}" alt="DC Logo">
+        </a>
+      </div>
+      {{-- Main menu --}}
+      <ul class="main-menu flex">
+        <li>
+          <a href="#">
+            CHARACTER
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            COMICS
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            MOVIES
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            TV
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            GAMES
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            COLLECTIBLES
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            VIDEOS
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            FANS
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            NEWS
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            SHOP
+          </a>
+        </li>
+      </ul>
+      {{-- Search bar --}}
+      <input type="text" placeholder="Search">
+    </div>
+  </nav>
+  {{-- Hero --}}
+  <div class="hero">
+    <div class="container-small">
+      <img src="{{ asset('images/cover-home.jpg') }}" alt="Cover home">
+    </div>
+  </div>
+</header>
